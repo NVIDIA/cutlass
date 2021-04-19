0b74c8f473e04871adef767c6d2bb1b8ee8f213b
KeDengMS <kedeng@microsoft.com>
2021-04-19 23:36:06 +0000

    Address CR

--- include/cutlass/epilogue/thread/linear_combination_gelu.h
+++ include/cutlass/epilogue/thread/linear_combination_gelu.h
@@ -134,6 +134,7 @@ public:
   /// Functionally required for serial reduction in the epilogue
   CUTLASS_HOST_DEVICE
   void set_k_partition(int k_partition, int k_partition_count) {
+    CUTLASS_UNUSED(k_partition_count);
     if (k_partition) {
       beta_ = ElementCompute(1);
     }
