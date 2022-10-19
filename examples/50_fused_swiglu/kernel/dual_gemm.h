01c7437808a23edff44dd596062e6157ce7bf345
danthe3rd <danthe3rd>
2022-10-19 09:28:27 +0000

    Ex50 wip

---
+++ examples/50_fused_swiglu/kernel/dual_gemm.h
@@ -0,0 +1,508 @@
+/***************************************************************************************************
+ * Copyright (c) 2017 - 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **************************************************************************************************/
+/*! \file
+    \brief Template for a pipelined GEMM kernel. Does not compute batching or support split-K.
+*/
+
+#pragma once
+
+#include "cutlass/cutlass.h"
+
+#include "cutlass/gemm/gemm.h"
+#include "cutlass/matrix_coord.h"
+#include "cutlass/semaphore.h"
+
+#include "../threadblock/dual_mma_multistage.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace cutlass {
+namespace gemm {
+namespace kernel {
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <
+  typename Mma0_,               ///! Threadblock-scoped matrix multiply-accumulate 
+  typename Mma1_,               ///! Threadblock-scoped matrix multiply-accumulate 
+  typename Epilogue0_,             ///! Epilogue
+  typename Epilogue1_,             ///! Epilogue
+  typename ThreadblockSwizzle_,   ///! Threadblock swizzling function
+  bool SplitKSerial               ///! If true, code supporting split-K via serial reduction is enabled.
+>
+struct DualGemm {
+
+  using Mma0 = Mma0_;
+  using Mma1 = Mma1_;
+  using DualMma = threadblock::DualMmaMultistage<
+    typename Mma0::Shape,
+    typename Mma0::IteratorA,
+    typename Mma0::SmemIteratorA,
+    Mma0::kCacheOpA,
+    typename Mma0::IteratorB,
+    typename Mma0::SmemIteratorB,
+    Mma0::kCacheOpB,
+    typename Mma0::ElementC,
+    typename Mma0::LayoutC,
+    typename Mma0::Policy,
+    Mma0::kStages,
+    SharedMemoryClearOption::kNone
+  >;
+
+  using Epilogue0 = Epilogue0_;
+  using Epilogue1 = Epilogue1_;
+  using OutputOp0 = typename Epilogue0::OutputOp;
+  using OutputOp1 = typename Epilogue1::OutputOp;
+  using ThreadblockSwizzle = ThreadblockSwizzle_;
+  static bool const kSplitKSerial = SplitKSerial;
+
+  /// Warp count (concept: GemmShape)
+  using WarpCount0 = typename Mma0::WarpCount;
+  static int const kThreadCount = 32 * WarpCount0::kCount;
+
+  /// Parameters structure
+  struct Params {
+    cutlass::gemm::GemmCoord problem_size_0;
+    cutlass::gemm::GemmCoord problem_size_1;
+    cutlass::gemm::GemmCoord grid_tiled_shape;
+    int swizzle_log_tile;
+
+    // Mma0
+    typename Mma0::IteratorA::Params params_A0;
+    typename Mma0::IteratorA::TensorRef ref_A0;
+    typename Mma0::IteratorB::Params params_B0;
+    typename Mma0::IteratorB::TensorRef ref_B0;
+    typename Epilogue0::OutputTileIterator::Params params_C0;
+    typename Epilogue0::OutputTileIterator::TensorRef ref_C0;
+    typename Epilogue0::OutputTileIterator::Params params_D0;
+    typename Epilogue0::OutputTileIterator::TensorRef ref_D0;
+    typename OutputOp0::Params output_op_0;
+
+    // Mma1
+    typename Mma1::IteratorB::Params params_B1;
+    typename Mma1::IteratorB::TensorRef ref_B1;
+    typename Epilogue1::OutputTileIterator::Params params_C1;
+    typename Epilogue1::OutputTileIterator::TensorRef ref_C1;
+    typename Epilogue1::OutputTileIterator::Params params_D1;
+    typename Epilogue1::OutputTileIterator::TensorRef ref_D1;
+    typename OutputOp1::Params output_op_1;
+
+    int *semaphore;
+    int gemm_k_iterations_0;
+    int gemm_k_size_0;
+    int gemm_k_iterations_1;
+    int gemm_k_size_1;
+
+    //
+    // Methods
+    //
+
+    CUTLASS_HOST_DEVICE
+    Params(): swizzle_log_tile(0), semaphore(0), gemm_k_iterations_0(0), gemm_k_size_0(0),
+        gemm_k_iterations_1(0), gemm_k_size_1(0) { }
+
+    CUTLASS_HOST_DEVICE
+    Params(
+      cutlass::gemm::GemmCoord const & problem_size_0,
+      cutlass::gemm::GemmCoord const & problem_size_1,
+      cutlass::gemm::GemmCoord const & grid_tiled_shape,
+      // Mma0: D0 = A @ B0 + C0
+      typename Mma0::IteratorA::TensorRef ref_A0,
+      typename Mma0::IteratorB::TensorRef ref_B0,
+      typename Epilogue0::OutputTileIterator::TensorRef ref_C0,
+      typename Epilogue0::OutputTileIterator::TensorRef ref_D0,
+      // Mma1: D1 = A @ B1 + C1
+      typename Mma1::IteratorB::TensorRef ref_B1,
+      typename Epilogue1::OutputTileIterator::TensorRef ref_C1,
+      typename Epilogue1::OutputTileIterator::TensorRef ref_D1,
+
+      typename OutputOp0::Params output_op_0 = typename OutputOp0::Params(),
+      typename OutputOp1::Params output_op_1 = typename OutputOp1::Params(),
+      int *workspace = nullptr
+    ):
+      problem_size_0(problem_size_0),
+      problem_size_1(problem_size_1),
+      grid_tiled_shape(grid_tiled_shape),
+      swizzle_log_tile(ThreadblockSwizzle().get_log_tile(grid_tiled_shape)),
+      // Mma0
+      params_A0(ref_A0.layout()),
+      ref_A0(ref_A0),
+      params_B0(ref_B0.layout()),
+      ref_B0(ref_B0),
+      params_C0(ref_C0.layout()),
+      ref_C0(ref_C0),
+      params_D0(ref_D0.layout()),
+      ref_D0(ref_D0),
+      // Mma1
+      params_B1(ref_B1.layout()),
+      ref_B1(ref_B1),
+      params_C1(ref_C1.layout()),
+      ref_C1(ref_C1),
+      params_D1(ref_D1.layout()),
+      ref_D1(ref_D1),
+      output_op_0(output_op_0),
+      output_op_1(output_op_1) {
+
+      int total_gemm_k_iterations_0 = (problem_size_0.k() + Mma0::Shape::kK - 1) / Mma0::Shape::kK;
+      int gemm_k_iterations_0 = (total_gemm_k_iterations_0 + grid_tiled_shape.k() - 1) / grid_tiled_shape.k();
+      gemm_k_size_0 = gemm_k_iterations_0 * Mma0::Shape::kK;
+      int total_gemm_k_iterations_1 = (problem_size_1.k() + Mma1::Shape::kK - 1) / Mma1::Shape::kK;
+      int gemm_k_iterations_1 = (total_gemm_k_iterations_1 + grid_tiled_shape.k() - 1) / grid_tiled_shape.k();
+      gemm_k_size_1 = gemm_k_iterations_1 * Mma1::Shape::kK;
+
+    semaphore = workspace;
+    }
+  };
+
+  /// Shared memory storage structure
+  union SharedStorage {
+    typename DualMma::SharedStorage main_loop;
+    typename Mma0::SharedStorage main_loop0;
+    typename Mma1::SharedStorage main_loop1;
+    typename Epilogue0::SharedStorage epilogue0;
+    typename Epilogue1::SharedStorage epilogue1;
+  };
+
+  //
+  // Methods
+  //
+
+  CUTLASS_HOST_DEVICE
+  DualGemm() { } 
+
+  /// Determines whether kernel satisfies alignment
+    static Status can_implement(
+      cutlass::gemm::GemmCoord const & problem_size_0,
+      cutlass::gemm::GemmCoord const & problem_size_1,
+      typename Mma0::IteratorA::TensorRef ref_A0,
+      typename Mma0::IteratorB::TensorRef ref_B0,
+      typename Epilogue0::OutputTileIterator::TensorRef ref_C0,
+      typename Epilogue0::OutputTileIterator::TensorRef ref_D0,
+      typename Mma1::IteratorB::TensorRef ref_B1,
+      typename Epilogue1::OutputTileIterator::TensorRef ref_C1,
+      typename Epilogue1::OutputTileIterator::TensorRef ref_D1) {
+
+    static int const kAlignmentA = Mma0::IteratorA::AccessType::kElements;
+    static int const kAlignmentB = Mma0::IteratorB::AccessType::kElements;
+    static int const kAlignmentC = Epilogue0::OutputTileIterator::kElementsPerAccess;
+
+    if (!TensorRef_aligned(ref_A0, kAlignmentA)) {
+      return Status::kErrorMisalignedOperand;
+    }
+
+    if (!TensorRef_aligned(ref_B0, kAlignmentB)) {
+      return Status::kErrorMisalignedOperand;
+    }
+
+    if (!TensorRef_aligned(ref_C0, kAlignmentC)) {
+      return Status::kErrorMisalignedOperand;
+    }
+
+    if (!TensorRef_aligned(ref_D0, kAlignmentC)) {
+      return Status::kErrorMisalignedOperand;
+    }
+
+    if (!TensorRef_aligned(ref_B1, kAlignmentB)) {
+      return Status::kErrorMisalignedOperand;
+    }
+
+    if (!TensorRef_aligned(ref_C1, kAlignmentC)) {
+      return Status::kErrorMisalignedOperand;
+    }
+
+    if (!TensorRef_aligned(ref_D1, kAlignmentC)) {
+      return Status::kErrorMisalignedOperand;
+    }
+
+    if ((problem_size_0.m() % kAlignmentA) || (problem_size_0.k() % kAlignmentA) ||
+      (problem_size_0.n() % kAlignmentB) || (problem_size_0.k() % kAlignmentB) ||
+      (problem_size_0.m() % kAlignmentC) || (problem_size_0.n() % kAlignmentC) ||
+      (problem_size_1.m() % kAlignmentA) || (problem_size_1.k() % kAlignmentA) ||
+      (problem_size_1.n() % kAlignmentB) || (problem_size_1.k() % kAlignmentB) ||
+      (problem_size_1.m() % kAlignmentC) || (problem_size_1.n() % kAlignmentC)) {
+
+      return Status::kErrorMisalignedOperand;
+    }
+
+    // Determine if fusion sizes are valid
+    if(problem_size_0.m() != problem_size_1.m())
+      return Status::kErrorInvalidProblem;
+
+    if(problem_size_0.n() != problem_size_1.n())
+      return Status::kErrorInvalidProblem;
+
+    return Status::kSuccess;
+  }
+
+  /// Executes one GEMM
+  CUTLASS_DEVICE
+  void operator()(Params const &params, SharedStorage &shared_storage) {
+
+    // Compute threadblock location
+    ThreadblockSwizzle threadblock_swizzle;
+
+    cutlass::gemm::GemmCoord threadblock_tile_offset =
+        threadblock_swizzle.get_tile_offset(params.swizzle_log_tile);
+
+    // Early exit if CTA is out of range
+    if (params.grid_tiled_shape.m() <= threadblock_tile_offset.m() ||
+      params.grid_tiled_shape.n() <= threadblock_tile_offset.n()) {
+
+      return;
+    }
+
+    // Compute initial location in logical coordinates
+    cutlass::MatrixCoord tb_offset_A0{
+      threadblock_tile_offset.m() * Mma0::Shape::kM,
+      threadblock_tile_offset.k() * params.gemm_k_size_0,
+    };
+
+    cutlass::MatrixCoord tb_offset_B0{
+      threadblock_tile_offset.k() * params.gemm_k_size_0,
+      threadblock_tile_offset.n() * Mma0::Shape::kN
+    };
+
+    cutlass::MatrixCoord tb_offset_B1{
+      threadblock_tile_offset.k() * params.gemm_k_size_1,
+      threadblock_tile_offset.n() * Mma1::Shape::kN
+    };
+
+    // Problem size is a function of threadblock index in the K dimension
+    int problem_size_k_0 = min(
+      params.problem_size_0.k(), 
+      (threadblock_tile_offset.k() + 1) * params.gemm_k_size_0);
+
+    // Compute threadblock-scoped matrix multiply-add
+    int gemm_k_iterations_0 = (problem_size_k_0 - tb_offset_A0.column() + Mma0::Shape::kK - 1) / Mma0::Shape::kK;
+
+    // Problem size is a function of threadblock index in the K dimension
+    int problem_size_k_1 = min(
+      params.problem_size_1.k(), 
+      (threadblock_tile_offset.k() + 1) * params.gemm_k_size_1);
+
+    // Compute threadblock-scoped matrix multiply-add
+    int gemm_k_iterations_1 = (problem_size_k_1 - tb_offset_B1.row() + Mma1::Shape::kK - 1) / Mma1::Shape::kK;
+
+
+    // Compute position within threadblock
+    int thread_idx = threadIdx.x;
+
+    // Construct iterators to A and B operands
+    typename Mma0::IteratorA iterator_A0(
+      params.params_A0,
+      params.ref_A0.data(),
+      {params.problem_size_0.m(), problem_size_k_0},
+      thread_idx,
+      tb_offset_A0);
+
+    typename Mma0::IteratorB iterator_B0(
+      params.params_B0,
+      params.ref_B0.data(),
+      {problem_size_k_0, params.problem_size_0.n()},
+      thread_idx,
+      tb_offset_B0);
+
+    typename Mma1::IteratorB iterator_B1(
+      params.params_B1,
+      params.ref_B1.data(),
+      {problem_size_k_1, params.problem_size_1.n()},
+      thread_idx,
+      tb_offset_B1);
+
+
+    // Broadcast the warp_id computed by lane 0 to ensure dependent code
+    // is compiled as warp-uniform.
+    int warp_idx = __shfl_sync(0x1f, threadIdx.x / 32, 0);
+    int lane_idx = threadIdx.x % 32;
+
+    //
+    // Main loop
+    //
+
+
+    // Construct thread-scoped matrix multiply
+    typename Mma0::FragmentC accum0;
+    typename Mma0::FragmentC accum1;
+    accum0.clear();
+    accum1.clear();
+    static constexpr bool kFuse = true;
+
+    if (kFuse) {
+      DualMma mma(shared_storage.main_loop, thread_idx, warp_idx, lane_idx);
+      if (!kSplitKSerial || gemm_k_iterations_0 > 0) {
+        // Compute threadblock-scoped matrix multiply-add
+        mma(gemm_k_iterations_0,
+          accum0, accum1,
+          iterator_A0, iterator_B0, iterator_B1,
+          accum0, accum1);
+      }
+    } else {
+      // Mma0
+      Mma0 mma0(shared_storage.main_loop0, thread_idx, warp_idx, lane_idx);
+      if (!kSplitKSerial || gemm_k_iterations_0 > 0) {
+        // Compute threadblock-scoped matrix multiply-add
+        mma0(gemm_k_iterations_0, accum0, iterator_A0, iterator_B0, accum0);
+      }
+
+      // Mma1
+      Mma1 mma1(shared_storage.main_loop1, thread_idx, warp_idx, lane_idx);
+      if (!kSplitKSerial || gemm_k_iterations_0 > 0) {
+        // Compute threadblock-scoped matrix multiply-add
+        mma1(gemm_k_iterations_1, accum1, iterator_A0, iterator_B1, accum1);
+      }
+    }
+
+    //
+    // Epilogue
+    //
+
+    OutputOp0 output_op_0(params.output_op_0);
+    OutputOp1 output_op_1(params.output_op_1);
+
+    //
+    // Masked tile iterators constructed from members
+    //
+
+    threadblock_tile_offset =
+        threadblock_swizzle.get_tile_offset(params.swizzle_log_tile);
+
+    //assume identity swizzle
+    MatrixCoord threadblock_offset(
+      threadblock_tile_offset.m() * Mma1::Shape::kM,
+      threadblock_tile_offset.n() * Mma1::Shape::kN
+    );
+
+    int block_idx = threadblock_tile_offset.m() + threadblock_tile_offset.n() * params.grid_tiled_shape.m();
+
+    // Construct the semaphore.
+    Semaphore semaphore(params.semaphore + block_idx, thread_idx);
+
+    // If performing a reduction via split-K, fetch the initial synchronization
+    if (kSplitKSerial && params.grid_tiled_shape.k() > 1) {
+      
+      // Fetch the synchronization lock initially but do not block.
+      semaphore.fetch();
+
+      // Indicate which position in a serial reduction the output operator is currently updating
+      output_op_0.set_k_partition(threadblock_tile_offset.k(), params.grid_tiled_shape.k());
+      output_op_1.set_k_partition(threadblock_tile_offset.k(), params.grid_tiled_shape.k());
+    }
+
+    // Tile iterator loading from source tensor.
+    typename Epilogue0::OutputTileIterator iterator_C0(
+      params.params_C0,
+      params.ref_C0.data(),
+      params.problem_size_0.mn(),
+      thread_idx,
+      threadblock_offset
+    );
+    typename Epilogue1::OutputTileIterator iterator_C1(
+      params.params_C1,
+      params.ref_C1.data(),
+      params.problem_size_1.mn(),
+      thread_idx,
+      threadblock_offset
+    );
+
+    // Tile iterator writing to destination tensor.
+    typename Epilogue0::OutputTileIterator iterator_D0(
+      params.params_D0,
+      params.ref_D0.data(),
+      params.problem_size_0.mn(),
+      thread_idx,
+      threadblock_offset
+    );
+    typename Epilogue1::OutputTileIterator iterator_D1(
+      params.params_D1,
+      params.ref_D1.data(),
+      params.problem_size_1.mn(),
+      thread_idx,
+      threadblock_offset
+    );
+
+    Epilogue0 epilogue0(
+      shared_storage.epilogue0,
+      thread_idx, 
+      warp_idx, 
+      lane_idx);
+    Epilogue1 epilogue1(
+      shared_storage.epilogue1,
+      thread_idx, 
+      warp_idx, 
+      lane_idx);
+
+    // Wait on the semaphore - this latency may have been covered by iterator construction
+    if (kSplitKSerial && params.grid_tiled_shape.k() > 1) {
+        
+      // For subsequent threadblocks, the source matrix is held in the 'D' tensor.
+      if (threadblock_tile_offset.k()) {
+        iterator_C0 = iterator_D0;
+        iterator_C1 = iterator_D1;
+      }
+
+      semaphore.wait(threadblock_tile_offset.k());
+
+      __threadfence();
+    }
+
+    // Execute the epilogue operator to update the destination tensor.
+    epilogue0(output_op_0, iterator_D0, accum0, iterator_C0); 
+    epilogue1(output_op_1, iterator_D1, accum1, iterator_C1); 
+    
+    //
+    // Release the semaphore
+    //
+
+    if (kSplitKSerial && params.grid_tiled_shape.k() > 1) {
+      
+      int lock = 0;
+      if (params.grid_tiled_shape.k() == threadblock_tile_offset.k() + 1) {
+
+        // The final threadblock resets the semaphore for subsequent grids.
+        lock = 0;
+      }
+      else {
+        // Otherwise, the semaphore is incremented
+        lock = threadblock_tile_offset.k() + 1;
+      }
+
+      __threadfence();
+      semaphore.release(lock);
+    }
+  }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+} // namespace kernel
+} // namespace gemm
+} // namespace cutlass
+
