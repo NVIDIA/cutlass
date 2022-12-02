d9a8e6e715a66e868ead00f7f66a53e838bceeb5
mikeiovine <mikeiovine@fb.com>
2022-12-01 20:15:38 -0800

    Fix MHA kernel
    Summary:
    
    ATT
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- examples/41_fused_multi_head_attention/kernel_forward.h
+++ examples/41_fused_multi_head_attention/kernel_forward.h
@@ -163,7 +163,9 @@ struct AttentionKernel {
     int32_t num_heads;
 
     CUTLASS_HOST_DEVICE int32_t o_strideM() const {
-      return head_dim_value;
+      // Note: Note in sync with cutlass' main branch!! Make sure to apply
+      // when updating cutlass.
+      return head_dim_value * num_heads;
     }
 
     // Moves pointers to what we should process
