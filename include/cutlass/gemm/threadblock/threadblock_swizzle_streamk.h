d298cf6b4aa7303299c86cb6107f0b98084d867a
Ali Hassani <alih@uoregon.edu>
2023-04-06 10:13:58 -0700

    Typos

--- include/cutlass/gemm/threadblock/threadblock_swizzle_streamk.h
+++ include/cutlass/gemm/threadblock/threadblock_swizzle_streamk.h
@@ -693,7 +693,7 @@ struct ThreadblockSwizzleStreamK {
     return GemmCoord(m, n, get_batch_idx());
   }
 
-  /// Obtains the calling threadblock's tiled coordinates for the given tile index (row-major rastorization)
+  /// Obtains the calling threadblock's tiled coordinates for the given tile index (row-major rasterization)
   CUTLASS_DEVICE
   GemmCoord get_tile_offset_row_major(int tile_idx) const
   {
@@ -741,7 +741,7 @@ struct ThreadblockSwizzleStreamK {
     div_mod_sk_iters_per_region(region_idx, iter_in_region, iter);
 
     int big_block_iters = (sk_big_blocks_per_region * sk_iters_per_normal_block()) + sk_big_blocks_per_region;   // number of iterations in the region's big blocks
-    int normal_block_iters = iter_in_region - big_block_iters;                                                 // number of iterations in the region's normal bocks
+    int normal_block_iters = iter_in_region - big_block_iters;                                                 // number of iterations in the region's normal blocks
 
     int big_block_idx_in_region = div_mod_sk_iters_per_big_block.div(iter_in_region);
     int normal_block_idx_in_region = sk_big_blocks_per_region + div_mod_sk_iters_per_normal_block.div(normal_block_iters);
