f71b7fa6954f5c25ebd212b56175612a875b440c
Matthias Jouanneaux <mjoux@nvidia.com>
2023-07-11 10:12:24 -0700

    update warpgroup_wait static_assert message

--- include/cute/arch/mma_sm90_gmma.hpp
+++ include/cute/arch/mma_sm90_gmma.hpp
@@ -59,7 +59,7 @@ CUTE_HOST_DEVICE
 void
 warpgroup_wait()
 {
-  static_assert(N >= 0 && N <= 7, "_warpgroup.wait {N}; must be in range [0, 7]");
+  static_assert(N >= 0 && N <= 7, "WGMMA wait: N must be in range [0, 7]");
 #if defined(CUTE_ARCH_MMA_SM90A_ENABLED)
   asm volatile("wgmma.wait_group.sync.aligned %0;\n" :: "n"(N) : "memory");
 #else
