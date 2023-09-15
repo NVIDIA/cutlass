eb01ba5500b8f146f1961e061197d140dc19a5fb
Vadim Markovtsev <gmarkhor@gmail.com>
2023-09-15 16:00:05 +0200

    Replace 0x1f with 0xffffffff in __shfl_sync
    This fixes compatibility with H100 and resolves #1094

--- examples/45_dual_gemm/kernel/dual_gemm.h
+++ examples/45_dual_gemm/kernel/dual_gemm.h
@@ -364,7 +364,7 @@ struct DualGemm {
 
     // Broadcast the warp_id computed by lane 0 to ensure dependent code
     // is compiled as warp-uniform.
-    int warp_idx = __shfl_sync(0x1f, threadIdx.x / 32, 0);
+    int warp_idx = __shfl_sync(0xffffffff, threadIdx.x / 32, 0);
     int lane_idx = threadIdx.x % 32;
 
     //
