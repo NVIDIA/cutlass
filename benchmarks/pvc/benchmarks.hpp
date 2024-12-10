eae5c5b269a041ddea62b8a2250770da57ace910
Finlay Marno <finlay.marno@codeplay.com>
2024-12-10 17:59:35 +0000

    add streamk benchmarks

--- benchmarks/pvc/benchmarks.hpp
+++ benchmarks/pvc/benchmarks.hpp
@@ -42,7 +42,7 @@ using PvcGemmBF16BF16FP32_RRR_1 = cutlass::gemm::device::GemmConfiguration<
         float, cutlass::layout::RowMajor,
         float, Shape<_256, _256, _32>,
         TiledMMA<MMAAtom, Layout<Shape<_8,_4,_1>>>,
-        XE_2D_U16x32x32_LD_N, XE_2D_U16x32x32_LD_V>;
+        XE_2D_U16x32x32_LD_N, XE_2D_U16x32x32_LD_V, void>;
 
 using PvcGemmBF16BF16FP32_RRR_2 = cutlass::gemm::device::GemmConfiguration<
         cutlass::arch::IntelPVC,
@@ -51,7 +51,7 @@ using PvcGemmBF16BF16FP32_RRR_2 = cutlass::gemm::device::GemmConfiguration<
         float, cutlass::layout::RowMajor,
         float, Shape<_128, _512, _32>,
         TiledMMA<MMAAtom, Layout<Shape<_4,_8,_1>>>,
-        XE_2D_U16x32x32_LD_N, XE_2D_U16x32x32_LD_V>;
+        XE_2D_U16x32x32_LD_N, XE_2D_U16x32x32_LD_V, void>;
 
 using PvcGemmBF16BF16FP32_RRR_3 = cutlass::gemm::device::GemmConfiguration<
         cutlass::arch::IntelPVC,
@@ -60,7 +60,7 @@ using PvcGemmBF16BF16FP32_RRR_3 = cutlass::gemm::device::GemmConfiguration<
         float, cutlass::layout::RowMajor,
                 float, Shape<_256, _128, _32>,
         TiledMMA<MMAAtom, Layout<Shape<_8,_4,_1>>>,
-        XE_2D_U16x32x32_LD_N, XE_2D_U16x32x32_LD_V>;
+        XE_2D_U16x32x32_LD_N, XE_2D_U16x32x32_LD_V, void>;
 
 using PvcGemmBF16BF16FP32_RRR_4 = cutlass::gemm::device::GemmConfiguration<
         cutlass::arch::IntelPVC,
@@ -69,7 +69,7 @@ using PvcGemmBF16BF16FP32_RRR_4 = cutlass::gemm::device::GemmConfiguration<
         float, cutlass::layout::RowMajor,
         float, Shape<_128, _256, _16>,
         TiledMMA<MMAAtom, Layout<Shape<_4,_8,_1>>>,
-        XE_2D_U16x32x16_LD_N, XE_2D_U16x16x32_LD_V>;
+        XE_2D_U16x32x16_LD_N, XE_2D_U16x16x32_LD_V, void>;
 
 using PvcGemmBF16BF16FP32_RRR_5 = cutlass::gemm::device::GemmConfiguration<
         cutlass::arch::IntelPVC,
@@ -78,7 +78,7 @@ using PvcGemmBF16BF16FP32_RRR_5 = cutlass::gemm::device::GemmConfiguration<
         float, cutlass::layout::RowMajor,
         float, Shape<_8, _128, _32>,
         TiledMMA<MMAAtom, Layout<Shape<_1,_4,_1>>>,
-        XE_2D_U16x8x32_LD_N, XE_2D_U16x32x32_LD_V>;
+        XE_2D_U16x8x32_LD_N, XE_2D_U16x32x32_LD_V, void>;
 
 CUTLASS_CREATE_GEMM_BENCHMARK(PvcGemmBF16BF16FP32_RRR_1);
 CUTLASS_CREATE_GEMM_BENCHMARK(PvcGemmBF16BF16FP32_RRR_2);
@@ -86,10 +86,23 @@ CUTLASS_CREATE_GEMM_BENCHMARK(PvcGemmBF16BF16FP32_RRR_3);
 CUTLASS_CREATE_GEMM_BENCHMARK(PvcGemmBF16BF16FP32_RRR_4);
 CUTLASS_CREATE_GEMM_BENCHMARK(PvcGemmBF16BF16FP32_RRR_5);
 
+using PvcGemmBF16BF16FP32_StreamK_RRR_1 = cutlass::gemm::device::GemmConfiguration<
+        cutlass::arch::IntelPVC,
+        cutlass::bfloat16_t, cutlass::layout::RowMajor,
+        cutlass::bfloat16_t, cutlass::layout::RowMajor,
+        float, cutlass::layout::RowMajor,
+        float, Shape<_256, _256, _32>,
+        TiledMMA<MMAAtom, Layout<Shape<_8,_4,_1>>>,
+        XE_2D_U16x32x32_LD_N, XE_2D_U16x32x32_LD_V,
+        cutlass::gemm::StreamKScheduler>;
+
+CUTLASS_CREATE_GEMM_BENCHMARK(PvcGemmBF16BF16FP32_StreamK_RRR_1);
+
 static void register_benchmarks() {
   CUTLASS_BENCHMARK(PvcGemmBF16BF16FP32_RRR_1);
   CUTLASS_BENCHMARK(PvcGemmBF16BF16FP32_RRR_2);
   CUTLASS_BENCHMARK(PvcGemmBF16BF16FP32_RRR_3);
   CUTLASS_BENCHMARK(PvcGemmBF16BF16FP32_RRR_4);
   CUTLASS_BENCHMARK(PvcGemmBF16BF16FP32_RRR_5);
+  CUTLASS_BENCHMARK(PvcGemmBF16BF16FP32_StreamK_RRR_1);
 }
--- benchmarks/pvc/gemm_configuration.hpp
+++ benchmarks/pvc/gemm_configuration.hpp
@@ -59,7 +59,8 @@ template<
   class ElementC, class LayoutC,
   class ElementAccumulator,
   class TileShape, class TiledMma,
-  class GmemTiledCopyA, class GmemTiledCopyB>
+  class GmemTiledCopyA, class GmemTiledCopyB,
+  class TileScheduler>
 struct GemmConfiguration {
   static_assert(sizeof(ElementA) == 0, "No valid GemmConfiguration configuration exists.");
 };
@@ -69,14 +70,14 @@ struct GemmConfiguration {
 // bfloat16
 
 template<typename LayoutA, typename LayoutB, typename LayoutC,
-  class TileShape, class TiledMma, class GmemTiledCopyA, class GmemTiledCopyB>
+  class TileShape, class TiledMma, class GmemTiledCopyA, class GmemTiledCopyB, class TileScheduler>
 struct GemmConfiguration<
       arch::IntelPVC,
       bfloat16_t, LayoutA,
       bfloat16_t, LayoutB,
       float, LayoutC,
       float, TileShape, TiledMma,
-      GmemTiledCopyA, GmemTiledCopyB> {
+      GmemTiledCopyA, GmemTiledCopyB, TileScheduler> {
   using DispatchPolicy = MainloopIntelPVC<3>;
 
   // Mainloop
@@ -111,7 +112,8 @@ struct GemmConfiguration<
   using GemmKernel = kernel::GemmUniversal<
     Shape<int, int, int, int>,
     CollectiveMainloop,
-    CollectiveEpilogue
+    CollectiveEpilogue,
+    TileScheduler
   >;
 
   using Gemm = GemmUniversalAdapter<GemmKernel>;
