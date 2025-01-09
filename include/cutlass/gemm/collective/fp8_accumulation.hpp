ef5620dd1d5e7c0af33083fae17b43c34f1d8950
Manish Gupta <mgupta.iitr@gmail.com>
2025-01-09 08:22:09 -0800

    Blockwise Scaling for FP8 (#1932)
    * F8 Blockwise Scaling
    
    * two more NumProducerThreadEvents
    
    ---------
    
    Co-authored-by: Haicheng Wu <haichengw@nvidia.com>

--- include/cutlass/gemm/collective/fp8_accumulation.hpp
+++ include/cutlass/gemm/collective/fp8_accumulation.hpp
@@ -37,10 +37,10 @@
 //////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////FP8 Accumulation///////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-/// It would promote (add) the results from the tensor core accumulators to the
-/// main accumulators when the number of MMAs reaches the max number of MMA
-/// interval specified by user, after that the tensor core accumulators are
-/// zeroed.
+/// This calss provides API to promote (add) or scale (multiply_add) the results 
+/// from the tensor core accumulators to the main accumulators when the number 
+/// of MMAs reaches the max number of MMA interval specified by user, after that
+/// the tensor core accumulators are zeroed.
 //////////////////////////////////////////////////////////////////////////////
 
 namespace cutlass::gemm::collective {
@@ -49,7 +49,8 @@ template <
     class EngineAccum,
     class LayoutAccum>
 struct GmmaFP8Accumulation {  
- using TensorAccum = cute::Tensor<EngineAccum, LayoutAccum>;
+  using TensorAccum = cute::Tensor<EngineAccum, LayoutAccum>;
+  using ElementAccumulator = typename EngineAccum::value_type;
 
   static_assert(is_static<LayoutAccum>::value, "Accumulator Layout should be static");
   static_assert(is_rmem<TensorAccum>::value , "Accumulator tensor must be rmem resident.");
@@ -63,12 +64,23 @@ private:
   uint32_t mma_count_;                        // current executed MMAs
   uint32_t reset_accum_flag_;                 // accum needs to be zeroed or not. 
 
+  // promote or `add` the partial accumulators to main accumulator (FADD).
   CUTLASS_DEVICE
   void promote_core() {
     warpgroup_wait<0>();
     CUTLASS_PRAGMA_UNROLL
     for (int i = 0; i < size(accum_); ++i) {
       accum_(i) += accum_temp_(i);
+    }
+  }
+
+  // `multiply` scale the partial accumulators and `add` to main accumulator (FFMA).
+  CUTLASS_DEVICE
+  void scale_core(ElementAccumulator const& scale) {
+    warpgroup_wait<0>();
+    CUTLASS_PRAGMA_UNROLL
+    for (int i = 0; i < size(accum_); ++i) {
+      accum_(i) += accum_temp_(i) * scale;
     }
   }
 
@@ -87,6 +99,10 @@ public:
     accum_temp_ = cute::make_fragment_like(accum);
   }
 
+  //
+  // Methods (Common)
+  //
+
   CUTLASS_DEVICE 
   TensorAccum& operator()() {
     return accum_temp_;
@@ -97,6 +113,10 @@ public:
   bool prepare_if_needed() { 
     return reset_accum_flag_;
   }
+
+  //
+  // Methods (for FADD version)
+  //
 
   /// promote (add) the results from the MMA accumulators to main accumulator if needed.
   CUTLASS_DEVICE
@@ -114,6 +134,29 @@ public:
   void promote_residue_if_needed() {
     if (__shfl_sync(0xffffffff, mma_count_ > 0, 0)) {
       promote_core();
+    }
+  }
+
+  //
+  // Methods (for FFMA version)
+  //
+
+  /// scale (multiply_add) the results from the MMA accumulators to main accumulator if needed.
+  CUTLASS_DEVICE
+  void scale_if_needed(ElementAccumulator const& scale) {
+    mma_count_ += mma_count_per_mainloop_iteration_;
+    reset_accum_flag_ = __shfl_sync(0xffffffff, mma_count_ == accum_promotion_interval_, 0);
+    if (reset_accum_flag_) {
+      scale_core(scale);
+      mma_count_ = 0;
+    }
+  }
+
+  /// scale (multiply_add) the residue results from the MMA accumulators to main accumulator if needed.
+  CUTLASS_DEVICE
+  void scale_residue_if_needed(ElementAccumulator const& scale) {
+    if (__shfl_sync(0xffffffff, mma_count_ > 0, 0)) {
+      scale_core(scale);
     }
   }
 };
