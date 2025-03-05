fd55abda2c033bcd7ac68ad11af22808aa32b7c2
Tadej Ciglarič <tadej.c@gmail.com>
2025-03-05 18:22:12 +0100

    Fix a bug with batch size (#244)
    Fix a bug with incorrect results when batch size > 1

--- include/cutlass/gemm/collective/xe_mma.hpp
+++ include/cutlass/gemm/collective/xe_mma.hpp
@@ -170,20 +170,18 @@ struct CollectiveMma<MainloopIntelPVC<Stages>, TileShape_, ElementA_, StrideA_, 
 
     auto [M,N,K,L] = problem_shape;
 
-    auto tiled_copy_a = make_tiled_copy(atom_load_A{}.with(
-                                   static_cast<ElementA const*>(args.ptr_A), M, K),
-                                   Layout<CopyThreadShape>{},
-                                   make_layout(shape_div(typename traits_load_A::BlockShape{}, CopyThreadShape{})));
-    auto tiled_copy_b = make_tiled_copy(atom_load_B{}.with(
-                                   static_cast<ElementB const*>(args.ptr_B), N, K),
-                                   Layout<CopyThreadShape>{},
-                                   make_layout(shape_div(typename traits_load_B::BlockShape{}, CopyThreadShape{})));
-
     auto mA_mkl = make_tensor(make_gmem_ptr(static_cast<ElementA const*>(args.ptr_A)),
                               make_layout(make_shape(M, K, L), args.dA));
 
     auto mB_nkl = make_tensor(make_gmem_ptr(static_cast<ElementB const*>(args.ptr_B)),
                               make_layout(make_shape(N, K, L), args.dB));
+
+    auto tiled_copy_a = make_tiled_copy(atom_load_A{}.with(mA_mkl),
+                                   Layout<CopyThreadShape>{},
+                                   make_layout(shape_div(typename traits_load_A::BlockShape{}, CopyThreadShape{})));
+    auto tiled_copy_b = make_tiled_copy(atom_load_B{}.with(mB_nkl),
+                                   Layout<CopyThreadShape>{},
+                                   make_layout(shape_div(typename traits_load_B::BlockShape{}, CopyThreadShape{})));
 
     return Params{tiled_copy_a, tiled_copy_b, mA_mkl, mB_nkl};
   }
@@ -227,7 +225,6 @@ struct CollectiveMma<MainloopIntelPVC<Stages>, TileShape_, ElementA_, StrideA_, 
     // Retile global tile for copies
     Tensor tAgA = thr_copy_A.retile_S(tCgA);
     Tensor tBgB = thr_copy_B.retile_S(tCgB);
-
 
 #if CUTLASS_ENABLE_DEBUG_PRINTS
     if (cutlass::thread(LOG_THREAD, LOG_GROUP)) {
