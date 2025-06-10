d2dedc6025f41f979494d766c0bf445ee12d3f76
Ruyman <ruyman@codeplay.com>
2025-06-10 21:13:34 +0100

    [SYCL] Workaround for Print bug on DPCPP 2025.1 (#413)
    Workaround for printf bug in DPC++ , to be removed once bug is in
    released compiler
    
    ---------
    
    Co-authored-by: Joe Todd <joseph.todd@intel.com>
    Co-authored-by: Muhammad Tanvir <muhammad.tanvir@codeplay.com>

--- include/cute/util/print.hpp
+++ include/cute/util/print.hpp
@@ -248,7 +248,12 @@ pretty_print(uint64_t v) {
 
 CUTE_HOST_DEVICE void
 pretty_print(float v) {
+//TODO(Codeplay) Remove this once DPC++ bugfix is in release.
+#if (SYCL_INTEL_TARGET)
+  printf("%*.2f", v);
+#else 
   printf("%*.2e", 10, v);
+#endif  // SYCL_INTEL_TARGET
 }
 
 CUTE_HOST_DEVICE void
