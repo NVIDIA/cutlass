10e765ff3646e7f6c909ad7a29e3240bb5dae79d
apuaaChen <34239936+apuaaChen@users.noreply.github.com>
2025-07-18 14:38:26 -0500

    Update evt_store_sm80_90.py

--- test/python/cutlass/evt/evt_store_sm80_90.py
+++ test/python/cutlass/evt/evt_store_sm80_90.py
@@ -73,31 +73,6 @@ class TestEVTStore(EVTTestCaseBase):
             
             break  # Only need to test once
 
-    @unittest.skipIf(device_cc() != 90, "This test is only for CC 90")
-    def test_invalid_store(self):
-        """
-        Test invalid store
-        """
-        def evt_invalid_store(accum):
-            D = accum
-            F = D + 1 # D has users, which is not allowed on SM90 or higher
-            return D, F
-        
-        for m, n, k, l in self.get_problem_sizes(8):
-            example_inputs = {
-                "accum": self.fake_tensor(self.element, (l, m, n)),
-                "D": self.fake_tensor(self.element, (l, m, n)),
-                "F": self.fake_tensor(self.element, (l, m, n))
-            }
-            with self.assertRaisesRegex(
-                    RuntimeError, 
-                    r"On SM90 or higher, D is expected to be a output node with 0 users " 
-                    r"to enable smem reuse between C and D, but got 1"
-                ):
-                launcher = EVTTestBed(self.element, evt_invalid_store, example_inputs)
-            
-            break  # Only need to test once
-
     def test_aux_store(self):
         """
         Returning a tensor with shape [m, n]
