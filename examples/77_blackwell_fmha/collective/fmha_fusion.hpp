61e6a2299d45cfc776e3c7b801744b8e8d987d41
Aya Z. Ibrahim <56703597+Aya-ZIbra@users.noreply.github.com>
2025-07-28 12:36:22 -0700

    Update fmha_fusion.hpp
    fixed flipped logic for isQBegin

--- examples/77_blackwell_fmha/collective/fmha_fusion.hpp
+++ examples/77_blackwell_fmha/collective/fmha_fusion.hpp
@@ -204,7 +204,7 @@ struct CausalMask : NoMask {
     // See note below on different ways to think about causal attention
     // Again, we'd add the offset_q into the max_blocks_q calculation
     int max_blocks_k = Base::get_trip_count(blk_coord, tile_shape, problem_size);
-    int offset_q = IsQBegin ? int(get<1>(problem_size)) - int(get<0>(problem_size)) : 0;
+    int offset_q = IsQBegin ? 0: int(get<1>(problem_size)) - int(get<0>(problem_size));
     int max_blocks_q = ceil_div(offset_q + (get<0>(blk_coord) + 1) * get<0>(tile_shape), get<1>(tile_shape));
     return std::min(max_blocks_k, max_blocks_q);
 
@@ -219,7 +219,7 @@ struct CausalMask : NoMask {
 
     int trip_count = get_trip_count(blk_coord, tile_shape, problem_size);
     int q_tile = min(get<0>(tile_shape), get<0>(problem_size) - get<0>(blk_coord) * get<0>(tile_shape));
-    int offset_tile_q = IsQBegin ? int((get<1>(problem_size)) - int(get<0>(problem_size))% get<1>(tile_shape)) : 0;
+    int offset_tile_q = IsQBegin ? 0: int((get<1>(problem_size)) - int(get<0>(problem_size))% get<1>(tile_shape));
     
     int masked_blocks = ceil_div(q_tile + offset_tile_q, get<1>(tile_shape));
     return std::min(masked_blocks, trip_count);
@@ -250,7 +250,7 @@ struct CausalMask : NoMask {
     //    - this is usually what we want for inference settings
     //      where we only compute the next row and use cache for the rest
     //    - if you'd like this, you only need to set kIsQBegin=false
-    int offset_q = IsQBegin ? int(get<1>(problem_size)) - int(get<0>(problem_size)) : 0;
+    int offset_q = IsQBegin ? 0: int(get<1>(problem_size)) - int(get<0>(problem_size));
     CUTLASS_PRAGMA_UNROLL
     for (int i = 0; i < size(acc_qk); i++) {
       auto pos = index_qk(i);
