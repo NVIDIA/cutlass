246cb42091b1ed1b89c1eac2312eed6625a754fd
HydraQYH <QYH820@Outlook.com>
2025-12-09 18:00:42 +0800

    Add todo.

--- include/cutlass/gemm/kernel/sm90_gemm_tma_warpspecialized_cooperative.hpp
+++ include/cutlass/gemm/kernel/sm90_gemm_tma_warpspecialized_cooperative.hpp
@@ -805,6 +805,7 @@ public:
           mainloop_pipe_consumer_state.advance(work_k_tile_count);
         }
 
+        // TODO: Equip the SM100 TileScheduler with is_last_tile
         if constexpr (!IsBlockScaled) {
           if (scheduler.is_last_tile(work_tile_info)) {
             // Hint on an early release of global memory resources.
